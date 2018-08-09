326e2276b2ad66d27102dd1f529f0bcd69a5d6f9
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-08-09 13:21:44 +0900

    prettier

--- src/App.js
+++ src/App.js
@@ -1,102 +1,111 @@
-import React, { Component } from 'react';
-import { Container, Header, Icon, Divider, Grid, List, Form, Button } from 'semantic-ui-react'
-import YoutubePlayer from 'react-youtube-player';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Icon,
+  Divider,
+  Grid,
+  List,
+  Form,
+  Button
+} from "semantic-ui-react";
+import YoutubePlayer from "react-youtube-player";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.ids = JSON.parse(localStorage.getItem('played_ids')) || []
-    this.state = {videoId: this.ids[0]}
+    this.ids = JSON.parse(localStorage.getItem("played_ids")) || [];
+    this.state = { videoId: this.ids[0] };
 
-    this.videoId = ''
-    this.historyMax = 10
+    this.videoId = "";
+    this.historyMax = 10;
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.clickId = this.clickId.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.clickId = this.clickId.bind(this);
   }
 
   onEnd(event) {
-    event.target.seekTo(0, true)
-    event.target.playVideo()
+    event.target.seekTo(0, true);
+    event.target.playVideo();
   }
 
   handleChange(event) {
-    this.videoId = event.target.value
+    this.videoId = event.target.value;
   }
 
   handleSubmit(event) {
-    this.setState({videoId: this.videoId})
-    this.updateHistory(this.videoId)
-    event.preventDefault()
+    this.setState({ videoId: this.videoId });
+    this.updateHistory(this.videoId);
+    event.preventDefault();
   }
 
   clickId(id, event) {
-    this.setState({videoId: id})
-    event.preventDefault()
+    this.setState({ videoId: id });
+    event.preventDefault();
   }
 
   updateHistory(id) {
     if (this.ids.includes(id)) {
-      return
+      return;
     }
 
     if (this.ids.length >= this.historyMax) {
-      this.ids.pop()
+      this.ids.pop();
     }
 
-    this.ids.unshift(id)
-    localStorage.setItem('played_ids', JSON.stringify(this.ids))
+    this.ids.unshift(id);
+    localStorage.setItem("played_ids", JSON.stringify(this.ids));
   }
 
   renderIdList(ids) {
-    const listIds = ids.map((id) =>
-      <List.Item key={id.toString()} as='a' onClick={(e) => this.clickId(id, e)}>{id}</List.Item>
-    )
+    const listIds = ids.map(id => (
+      <List.Item key={id.toString()} as="a" onClick={e => this.clickId(id, e)}>
+        {id}
+      </List.Item>
+    ));
 
-    return (
-      <List>{listIds}</List>
-    )
+    return <List>{listIds}</List>;
   }
 
   render() {
     return (
       <Container>
-        <Header as='h2' icon textAlign='center' color='teal'>
-          <Icon name='play' />
+        <Header as="h2" icon textAlign="center" color="teal">
+          <Icon name="play" />
           <Header.Content>YouTube Repeater</Header.Content>
         </Header>
         <Divider hidden section />
-        <Form onSubmit={this.handleSubmit} >
+        <Form onSubmit={this.handleSubmit}>
           <Form.Field>
-            <input placeholder='video ID' onChange={this.handleChange} required='true' />
+            <input
+              placeholder="video ID"
+              onChange={this.handleChange}
+              required="true"
+            />
           </Form.Field>
-          <Button type='submit' >Play</Button>
+          <Button type="submit">Play</Button>
         </Form>
         <Divider hidden section />
         <Grid>
           <Grid.Column width={12}>
-            <div className='ui embed'>
-              {this.state.videoId &&
+            <div className="ui embed">
+              {this.state.videoId && (
                 <YoutubePlayer
                   videoId={this.state.videoId}
-                  playbackState='playing'
+                  playbackState="playing"
                   onEnd={this.onEnd}
-                  configuration={
-                    {
-                      autoplay: 1,
-                      showinfo: 0
-                    }
-                  }
+                  configuration={{
+                    autoplay: 1,
+                    showinfo: 0
+                  }}
                 />
-              }
+              )}
             </div>
           </Grid.Column>
-          <Grid.Column width={3}>
-            {this.renderIdList(this.ids)}
-          </Grid.Column>
+          <Grid.Column width={3}>{this.renderIdList(this.ids)}</Grid.Column>
         </Grid>
       </Container>
     );
