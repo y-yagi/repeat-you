5faa2ca3c119e23fb21c1551bbc0f4a6b9b0b2d2
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-12-16 16:24:01 +0900

    Don't use SSR
    This application depends on `window`. So it wouldn't work in SSR.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
 import type { AppProps } from "next/app";
+import CSR from "../components/csr";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CSR>
+      <Component {...pageProps} />
+    </CSR>
+  );
 }
 
 export default MyApp;
